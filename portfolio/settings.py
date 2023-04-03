a3ebda0f5e2a6c32df534e517f5866ae3878f4de
shikhin <shikhindah@gmail.com>
2023-04-04 01:50:50 +0530

    fail safe

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -22,11 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+try:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
-if not SECRET_KEY:
-    load_dotenv(os.path.join('', '.env'))
-    SECRET_KEY = os.environ['SECRET_KEY']
+    if not SECRET_KEY:
+        load_dotenv(os.path.join('', '.env'))
+        SECRET_KEY = os.environ['SECRET_KEY']
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
